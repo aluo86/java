4faa059f140af7ea6ab3cd8d5c458dae7490c93d
aluo86 <andrewluo@gmail.com>
2014-03-30 22:40:58 -0700

    Added java preference api demo

---
+++ demo/misc/src/com/github/aluo86/preferences/PreferenceDemo.java
@@ -0,0 +1,45 @@
+package com.github.aluo86.preferences;
+
+import java.io.OutputStream;
+import java.util.prefs.Preferences;
+
+public class PreferenceDemo {
+	private Preferences prefs;
+
+	  public void setPreference() {
+	    // This will define a node in which the preferences can be stored
+	    prefs = Preferences.userRoot().node(this.getClass().getName());
+	    String ID1 = "Test1";
+	    String ID2 = "Test2";
+	    String ID3 = "Test3";
+
+	    // First we will get the values
+	    // Define a boolean value
+	    System.out.println(prefs.getBoolean(ID1, true));
+	    // Define a string with default "Hello World
+	    System.out.println(prefs.get(ID2, "Hello World"));
+	    // Define a integer with default 50
+	    System.out.println(prefs.getInt(ID3, 50));
+
+	    // now set the values, it will save the pref in windows registry table
+	    // or systemRoot: HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft...
+	    //for userRoot: HKEY_CURRENT_USER\SOFTWARE\JavaSoft...
+	    prefs.putBoolean(ID1, false);
+	    prefs.put(ID2, "Hello Europa");
+	    prefs.putInt(ID3, 45);
+
+	    // Delete the preference settings for the first value
+	    prefs.remove(ID1);
+	    
+	    //print new value
+	    System.out.println(prefs.get(ID2, "Hello World"));
+	    System.out.println(prefs.getInt(ID3, 12));
+	    
+
+	  }
+
+	  public static void main(String[] args) {
+	    PreferenceDemo demo = new PreferenceDemo();
+	    demo.setPreference();
+	  }
+}
